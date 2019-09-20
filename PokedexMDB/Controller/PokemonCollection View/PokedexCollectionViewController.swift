626a8f86035166f9249b4ed4578980cafe69ebab
Shaina <shaina.sierra@gmail.com>
2019-09-19 17:00:18 -0700

    first commit

---
+++ PokedexMDB/Controller/PokemonCollection View/PokedexCollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokedexCollectionViewController.swift
+//  PokedexMDB
+//
+//  Created by shaina on 9/19/19.
+//  Copyright © 2019 shaina. All rights reserved.
+//
+
+import UIKit
+
+class PokedexCollectionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
